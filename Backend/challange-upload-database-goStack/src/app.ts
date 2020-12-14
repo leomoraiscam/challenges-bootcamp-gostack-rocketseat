ae8c23043a23a1fe3bbee1ce5bf9f21be35a2fe9
leomoraiscam <leozonnn@gmail.com>
2020-12-14 20:03:06 -0300

    update challange

--- Backend/challange-upload-database-goStack/src/app.ts
+++ Backend/challange-upload-database-goStack/src/app.ts
@@ -1,9 +1,8 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-
+import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
 
@@ -13,6 +12,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
